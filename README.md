271150e0f535a0540d6c3be3c1683645a6de2cd8
luungoc2005 <luungoc2005@gmail.com>
2018-01-29 23:30:26 +0700

    update README & BiLSTM model

--- README.md
+++ README.md
@@ -14,7 +14,17 @@ botbot-nlp
 - Install dependencies by running `pip install -r requirements.txt`
 - Install any missing dependencies (because of platform differences)
 
-3. Using Jupyter notebook for evaluation
+3. Install HDF5 for Keras model saving/loading
+
+4. Download NLTK data:
+- Run `python`
+- Type the following into the prompt:
+```
+import nltk
+
+```
+
+5. Using Jupyter notebook for evaluation
 - Run `jupyter notebook`. A browser tab should open and navigate to jupyter notebook at `localhost:8888` by default
 - Open `entities_train.ipynb` inside the notebook
 - Click Kernel > Run All to start training
@@ -28,4 +38,4 @@ botbot-nlp
 # TODOS
 - Try replacing ConvNet Dense layers by AveragePooling layers for reduced number of parameters (faster training)
 - Implement transfer learning (Dropping the last Dense layer & Modifying Embedding layer to reuse old weights)
-- 
+- Explore RNN/LSTM-based architectures for classification
--- bilstm/model.py
+++ bilstm/model.py
@@ -1,7 +1,27 @@
 from keras.layers import Input, LSTM, Bidirectional, Dense, Dropout, concatenate
+from keras.layers.wrappers import TimeDistributed
+from keras.models import Model
 
 RECURRENT_DEPTH = 2
 RECURRENT_UNITS = 32
 
-def EntitiesBiLSTM():
-    
+def EntitiesBiLSTM(num_classes):
+    input_tokens, input_pos
+    tokens_lstm_layers = list(range(RECURRENT_DEPTH))
+    pos_lstm_layers = list(range(RECURRENT_DEPTH))
+
+    for idx in range(RECURRENT_DEPTH):
+        tokens_lstm_layers[idx] = Dropout(0.3)(input_tokens if idx == 0 else tokens_lstm_layers[idx-1])
+        tokens_lstm_layers[idx] = Bidirectional(LSTM(RECURRENT_UNITS, return_sequences=True))(tokens_lstm_layers[idx])
+        tokens_lstm_layers[idx] = Dropout(0.3)(tokens_lstm_layers[idx])
+
+        pos_lstm_layers[idx] = Dropout(0.3)(input_pos if idx == 0 else pos_lstm_layers[idx-1])
+        pos_lstm_layers[idx] = Bidirectional(LSTM(RECURRENT_UNITS, return_sequences=True))(pos_lstm_layers[idx])
+        pos_lstm_layers[idx] = Dropout(0.3)(pos_lstm_layers[idx])
+
+    x = concatenate([tokens_lstm_layers[-1], pos_lstm_layers[-1]])
+    output = TimeDistributed(Dense(num_classes, activation='softmax'))(x)
+
+    model = Model(inputs=[input_tokens, input_pos], outputs=[output])
+
+    return model
