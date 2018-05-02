9157920c44778483b59ad822d443e1dc4604f7d5
luungoc2005 <luungoc2005@gmail.com>
2018-05-02 21:29:30 +0700

    reduce batch size

--- train_script.py
+++ train_script.py
@@ -1,6 +1,6 @@
 import torch
 from sent_to_vec.train import trainIters
 
-encoder, nli_net = trainIters(batch_size=32)
+encoder, nli_net = trainIters(batch_size=24)
 torch.save(encoder.state_dict(), 'encoder.bin')
 torch.save(nli_net.state_dict(), 'nli_net.bin')
