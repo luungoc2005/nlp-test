a302310f487f58d26b113fd744b09db02dcd2763
luungoc2005 <luungoc2005@gmail.com>
2018-05-07 11:29:08 +0700

    model skeleton

---
+++ paraphrase_vae/model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from config import MAX_NUM_WORDS
+
+
+class EncoderRNN(nn.Module):
+
+    def __init__():
+        super(EncoderRNN, self).__init__()
+
+class AttnDecoderRNN(nn.Module):
+    
+    def __init__():
+        super(AttnDecoderRNN, self).__init__()
