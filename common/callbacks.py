023856fc63cae4a5a739278c7d3a8644795b6e0d
luungoc2005 <luungoc2005@gmail.com>
2018-06-20 15:41:05 +0700

    WIP / refactoring

---
+++ common/callbacks.py
@@ -0,0 +1,25 @@
+from abc import ABCMeta, abstractmethod
+
+
+class ICallback(metaclass=ABCMeta):
+
+    def __init__(self):
+        self._learner = None
+
+    def set_learner(self, learner):
+        self._learner = learner
+
+    @property
+    def learner(self): return self._learner
+
+    @abstractmethod
+    def on_training_start(self): pass
+
+    @abstractmethod
+    def on_epoch_start(self): pass
+
+    @abstractmethod
+    def on_epoch_end(self): pass
+
+    @abstractmethod
+    def on_training_end(self): pass
