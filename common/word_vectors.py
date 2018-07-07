4e7f35cdb450a5b447867865aa14f7ee8bcbde87
luungoc2005 <luungoc2005@gmail.com>
2018-07-07 14:57:59 +0700

    Revert Magnitude to disable lazy loading

--- common/word_vectors.py
+++ common/word_vectors.py
@@ -1,8 +1,8 @@
 from pymagnitude import *
 from config import MAGNITUDE_PATH, MAX_NUM_WORDS
 
-# vectors = Magnitude(MAGNITUDE_PATH, lazy_loading=-1, case_insensitive=True)
-vectors = Magnitude(MAGNITUDE_PATH, lazy_loading=1, case_insensitive=True)
+vectors = Magnitude(MAGNITUDE_PATH, lazy_loading=-1, case_insensitive=True)
+# vectors = Magnitude(MAGNITUDE_PATH, lazy_loading=1, case_insensitive=True)
 
 def get_emb_matrix():
     return vectors.get_vectors_mmap()[:MAX_NUM_WORDS]
