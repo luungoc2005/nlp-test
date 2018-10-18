dbd5908458c094618c2922469215ed4afbb8d972
luungoc2005 <luungoc2005@gmail.com>
2018-10-18 12:30:18 +0700

    reduce dims to fit in GPU

--- train_awd_lm.py
+++ train_awd_lm.py
@@ -5,19 +5,19 @@ from os import path
 from config import BASE_PATH
 from torch.optim import RMSprop
 
-model = LanguageModelWrapper({'embedding_dim': 768}) # small model
+model = LanguageModelWrapper({'embedding_dim': 512}) # small model
 
 dataset = WikiTextDataset()
 
 SAVE_PATH = path.join(BASE_PATH, 'wikitext-data.bin')
 if path.exists(SAVE_PATH):
     print('Loading from previously saved file')
-    dataset.load(SAVE_PATH, model, batch_size=8)
+    dataset.load(SAVE_PATH, model, batch_size=16)
 else:
     dataset.initialize(model, data_path=[
         path.join(BASE_PATH, 'data/wikitext2/wiki.train.tokens'),
         path.join(BASE_PATH, 'data/wikitext103/wiki.train.tokens')
-    ], batch_size=8)
+    ], batch_size=16)
     dataset.save()
 
 learner = LanguageModelLearner(model, optimizer_fn='rmsprop')
