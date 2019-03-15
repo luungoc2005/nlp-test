c2c1ee5472f7168fd3707a217b79b489629397d5
luungoc2005 <luungoc2005@gmail.com>
2019-03-16 01:55:55 +0800

    fix decoder to be compatible with old LM

--- sent_to_vec/masked_lm/transformer.py
+++ sent_to_vec/masked_lm/transformer.py
@@ -360,14 +360,15 @@ class BertLMPredictionHead(nn.Module):
         # The output weights are the same as the input embeddings, but there is
         # an output-only bias for each token.
         self.decoder = nn.Linear(bert_model_embedding_weights.size(1),
-                                 bert_model_embedding_weights.size(0),
-                                 bias=False)
+                                 bert_model_embedding_weights.size(0))
+                                #  bias=False)
+        self.decoder.bias.data.zero_()
         self.decoder.weight = bert_model_embedding_weights
-        self.bias = nn.Parameter(torch.zeros(bert_model_embedding_weights.size(0)))
+        # self.bias = nn.Parameter(torch.zeros(bert_model_embedding_weights.size(0)))
 
     def forward(self, hidden_states):
         hidden_states = self.transform(hidden_states)
-        hidden_states = self.decoder(hidden_states) + self.bias
+        # hidden_states = self.decoder(hidden_states) + self.bias
         return hidden_states
 
 
