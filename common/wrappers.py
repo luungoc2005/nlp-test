66f5e0544688ce6ccb8ae7dd2d862d8472fb75bf
luungoc2005 <luungoc2005@gmail.com>
2019-03-16 12:52:50 +0800

    more checks

--- common/wrappers.py
+++ common/wrappers.py
@@ -264,7 +264,7 @@ class IModel(object):
         raise NotImplementedError
 
     @property
-    def model(self): return self._model.module if self._use_data_parallel == True else self._model
+    def model(self): return self._model.module if (self._use_data_parallel == True and self._model is not None and hasattr(self._model, 'module')) else self._model
 
     @property
     def training(self): return self._model.training if hasattr(self._model, 'training') else False
