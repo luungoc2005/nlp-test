837969539a1f59343c5f9bf7eaa1b957e441403d
luungoc2005 <luungoc2005@gmail.com>
2019-06-02 19:44:54 +0800

    re-added entrypoint commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# . botbot-env/bin/activate
+. botbot-env/bin/activate
 
 # pip install nltk Flask pymagnitude scikit-learn scipy gunicorn
 # pip install http://download.pytorch.org/whl/cpu/torch-0.4.1-cp36-cp36m-linux_x86_64.whl 
 # python3 -m nltk.downloader 'punkt'
 
-# cd ./botbot-nlp
+cd ./botbot-nlp
 
 CUDA_VISIBLE_DEVICES=-1
 LOG_FILE="./flask_app/logs/main_$(date +%s)_stdout.log"
