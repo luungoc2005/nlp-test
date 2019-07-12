various fixes; added inhibited softmax for uncertainty handling

text_classification/utils/inference.py
@@ -32,6 +32,8 @@ def infer_classification_output(
     except:
         pass
 
+    logits = logits.cpu()
+
     topk = topk or model_topk
     batch_size = logits.size(0)
     n_classes = model.model.n_classes if model.is_pytorch_module() else model.n_classes
@@ -62,14 +64,26 @@ def infer_classification_output(
     top_probs, top_idxs = torch.topk(logits, topk)
 
     top_idxs = top_idxs.numpy()
-    top_classes = [
-        model.label_encoder.inverse_transform(top_idxs[idx])
-        for idx in range(batch_size)
-    ]
+    # top_classes = [
+    #     model.label_encoder.inverse_transform(top_idxs[idx])
+    #     for idx in range(batch_size)
+    # ]
+    top_classes = []
+    # workaround for LabelEncoder
+    labels_dict = dict(zip(model.label_encoder.classes_, model.label_encoder.transform(model.label_encoder.classes_)))
+
+    for idx in range(batch_size):
+        try:
+            sent_top_classes = []
+            for class_idx in range(topk):
+                sent_top_classes.append(labels_dict.get(top_idxs[class_idx], '<unknown>'))
+            top_classes.append(sent_top_classes)
+        except:
+            top_classes.append(None)
     return [
         [{
             'intent': top_classes[sent_idx][idx],
             'confidence': top_probs[sent_idx][idx].item()
         } for idx in range(topk)
-        if top_probs[sent_idx][idx].item() != 0]
+        if top_probs[sent_idx] is not None and top_probs[sent_idx][idx].item() != 0]
         for sent_idx in range(batch_size)]

text_classification/with_pretrained/model.py
@@ -16,14 +16,14 @@ class LMClassifier(nn.Module):
 
     def __init__(self, config=dotdict({
         'encoder_dropout': .2,
-        'hidden_size': 512,
+        'hidden_size': 1024,
         'rnn_layers': 1,
         'pool_hidden_size': 512,
-        'pool_layers': 1,
+        'pool_layers': 3,
         'pool_act': 'cauchy'
     }), *args, **kwargs):
         super(LMClassifier, self).__init__(*args, **kwargs)
-        self.encoder = config.get('encoder', BertLMWrapper)
+        self.encoder = None
         
         self.encoder_size = config.get('encoder_size', \
             self.encoder.config.get('hidden_size', 512) \
@@ -114,10 +114,11 @@ class LMClassifier(nn.Module):
 
 class LMClassifierWrapper(IModel):
 
-    def __init__(self, config={}):
+    def __init__(self, config={}, *args, **kwargs):
         super(LMClassifierWrapper, self).__init__(
             model_class=LMClassifier, 
-            config=config
+            config=config,
+            *args, **kwargs
         )
         self.config = config
 
@@ -127,16 +128,16 @@ class LMClassifierWrapper(IModel):
 
     def get_state_dict(self):
         return {
-            'label_encoder': self.label_encoder,
+            'label_encoder': self.label_encoder
         }
 
     def load_state_dict(self, state_dict):
-        # re-initialize model with loaded config
-        self.model = self.init_model()
-        self.model.set_params(state_dict['state_dict'])
-
         # load label encoder
         self.label_encoder = state_dict['label_encoder']
+
+    def on_model_init(self):
+        self.model.encoder = self.config['encoder'].model
+        self._featurizer = self.config['encoder']._featurizer
 
     def preprocess_output(self, y):
         # One-hot encode outputs